dc18021ec8b5d5a921a722c89575bf1be2f4a68a
BrokenR3C0RD <shadowcx11sb@gmail.com>
2016-09-24 12:50:06 -0300

    Modify some things

--- data/baseStats/ditto.asm
+++ data/baseStats/ditto.asm
@@ -6,17 +6,17 @@ db 48 ; base speed
 db 48 ; base special
 db NORMAL ; species type 1
 db NORMAL ; species type 2
-db 35 ; catch rate
+db 255 ; catch rate
 db 61 ; base exp yield
 INCBIN "pic/ymon/ditto.pic",0,1 ; 55, sprite dimensions
 dw DittoPicFront
 dw DittoPicBack
 ; attacks known at lvl 0
 db TRANSFORM
+db HYPER_BEAM
 db 0
 db 0
-db 0
-db 0 ; growth rate
+db 255 ; growth rate
 ; learnset
 	tmlearn 0
 	tmlearn 0
--- data/item_prices.asm
+++ data/item_prices.asm
@@ -2,7 +2,7 @@ ItemPrices:
 	money 0     ; MASTER_BALL
 	money 1200  ; ULTRA_BALL
 	money 600   ; GREAT_BALL
-	money 200   ; POKE_BALL
+	money 0     ; POKE_BALL
 	money 0     ; TOWN_MAP
 	money 0     ; BICYCLE
 	money 0     ; SURFBOARD
